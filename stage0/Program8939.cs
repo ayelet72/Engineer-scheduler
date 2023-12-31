e69626f32945647daf56c1fe42090eab3f041b9f
Ayelet Hashachar <shacharabayev@gmail.com>
2023-12-31 15:46:37 +0200

    Initial Code + Welcome functions: stage0 of the project :)

---
+++ stage0/Program8939.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+
+namespace stage0
+{
+    partial class Program
+    { 
+        
+        private static void Main(string[] args)
+        {
+            welcome8939();
+            welcome3029();
+            Console.ReadKey();
+        }
+        private static void welcome8939()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} welcome to my first console application ", name);
+        }
+        static partial void welcome3029(); 
+
+    }
+}
+
+
+

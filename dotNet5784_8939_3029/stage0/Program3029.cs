fee14c0e5087ab7b6235406d6386b0fbcb1a2185
HadarMahasri <hadarhadar690@gmail.com>
2023-12-31 18:41:12 +0200

    im here

---
+++ dotNet5784_8939_3029/stage0/Program3029.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace stage0
+{
+    public partial class program3029
+    {
+        static partial void welcome3029()
+        {
+            Console.WriteLine("I am also here!");
+
+        }
+    }
+
+}

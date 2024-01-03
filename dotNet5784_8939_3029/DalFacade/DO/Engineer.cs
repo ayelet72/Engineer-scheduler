566758ac87ca7721ee8d716eda511a19b9df1989
Ayelet Hashachar <shacharabayev@gmail.com>
2024-01-03 10:43:58 +0200

    starting stage 1 together

--- dotNet5784_8939_3029/DalFacade/DO/Engineer.cs
+++ dotNet5784_8939_3029/DalFacade/DO/Engineer.cs
@@ -4,25 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DO
+namespace DO;                   
+
+/// <summary>
+/// Engineer involved in projects with milestones
+/// <see>Monday</see>
+/// </summary>
+/// <param name="ID"> int as identifier</param>
+/// <param name="EMail">email of the worker</param>
+/// <param name="Name">name of the worker</param>
+/// <param name="Cost">"alibaba and 40 thieves</param>
+/// <param name="Level">Experience level</param>
+/// <param name="Active">ana aref</param>
+public record Engineer
+(
+    int ID,
+    string EMail,
+    string Name,
+    double Cost,
+    EngineerExperience Level = EngineerExperience.Beginner,
+    bool Active = true
+)
 {
+    public Engineer() : this(0,"","",0) { } //empty ctor for stage 3
+
     /// <summary>
-    /// Engineer involved in projects with milestones
-    /// <see>Monday</see>
+    /// RegistrationDate - registration date of the current student record
     /// </summary>
-    /// <param name="ID"> int as identifier</param>
-    /// <param name="EMail">email of the worker</param>
-    /// <param name="Name">name of the worker</param>
-    /// <param name="Cost">"alibaba and 40 thieves</param>
-    /// <param name="Level">Experience level</param>
-    /// <param name="Active">ana aref</param>
-    public record Engineer
-    (
-        int ID,
-        string EMail,
-        string Name,
-        double Cost,
-        EngineerExperience Level = EngineerExperience.Beginner,
-        bool Active = true
-    );
+    //public Engineer(int id, string email, string name, double cost):this(id,email,name,cost)
+   // {
+        //this.ID = id;
+        //this.EMail = email;
+        //this.Name = name;
+        //this.Cost = cost;
+
+    
+   // } //get only
 }
+
